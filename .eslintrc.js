b63ea0d84aecb1b8eb4ef20e9bae96b85aa5df42
White Piano <startoflateness@gmail.com>
2023-10-19 21:26:15 +0900

    tsc 버전업 (skipLibCheck 미지원)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,12 @@
 module.exports = {
-    extends: ["airbnb-typescript/base", "prettier"],
-    reportUnusedDisableDirectives: true,
-    rules: {
-        "max-len": ["warn", { code: 140 }],
-        quotes: [2, "single", { avoidEscape: true }],
-        "@typescript-eslint/indent": ["error", 4],
-        "@typescript-eslint/no-unused-vars": [
-            "warn",
-            { argsIgnorePattern: "^_", ignoreRestSiblings: true },
-        ],
-    },
+  extends: ['airbnb-typescript/base', 'prettier'],
+  reportUnusedDisableDirectives: true,
+  rules: {
+    'max-len': ['warn', { code: 140 }],
+    quotes: [2, 'single', { avoidEscape: true }],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', ignoreRestSiblings: true },
+    ],
+  },
 };
